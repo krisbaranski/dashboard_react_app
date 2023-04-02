a0ef58d7f6601d49c74c9fa8e70ab06725e80514
krzysztof baranski <krisbaranski@gmail.com>
2023-04-02 21:57:30 +0200

    refactor: add Calendar Sidebar

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -47,6 +47,48 @@ const Calendar = () => {
     }
   };
 
+  return (
+    <Box m='20px'>
+      <Header title='CALENDAR' subtitle='Full Calendar Interative Page' />
 
+      <Box display='flex' justifyContent='space-between'>
+        {/* CALENDAR SIDEBAR */}
+        <Box
+          flex='1 1 20%'
+          backgroudColor={colors.primary[400]}
+          p='15px'
+          borderRadius='4px'
+        >
+          <Typography variant='h5'>Events</Typography>
+          <List>
+            {currentEvents.map((event) => (
+              <ListItem
+                key={event.id}
+                sx={{
+                  backgroudColor: colors.greenAccent[500],
+                  margin: "10px 0",
+                  borderRadius: "2px",
+                }}
+              >
+                <ListItemText
+                  primary={event.title}
+                  secondary={
+                    <Typography>
+                      {formatDate(event.start, {
+                        year: "numeric",
+                        month: "short",
+                        day: "numeric",
+                      })}
+                    </Typography>
+                  }
+                />
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
 
 export default Calendar;
