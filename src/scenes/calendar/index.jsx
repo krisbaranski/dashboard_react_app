e7652436f078669427a6fbbf5d6b211e5cb68796
krzysztof baranski <krisbaranski@gmail.com>
2023-04-02 21:20:01 +0200

    refactor: add click handlers and functionality

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -18,5 +18,35 @@ import { tokens } from "../../theme";
 const Calendar = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
-};
+  const [currentEvents, setCurrentEvents] = useState([]);
+
+  const handleDateClick = (selected) => {
+    const title = prompt("Please enter a new title for your event");
+
+    const calendarApi = selected.view.calendar;
+    calendarApi.unselect();
+
+    if (title) {
+      calendarApi.addEvent({
+        id: `${selected.dateStr}-${title}`,
+        title,
+        start: selected.startStr,
+        end: selected.endStr,
+        allDay: selected.allDay,
+      });
+    }
+  };
+
+  const handleEventClick = (selected) => {
+    if (
+      window.confirm(
+        `Are you sure you want to delete the event '${selected.event.title}' ?`
+      )
+    ) {
+      selected.event.remove();
+    }
+  };
+
+
+
 export default Calendar;
