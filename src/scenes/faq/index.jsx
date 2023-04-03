7220b99075b21c37eb8124d12428d2e44634bcb5
krzysztof baranski <krisbaranski@gmail.com>
2023-04-03 20:43:26 +0200

    refactor: correct imports, add accordion elements with lorem text

--- src/scenes/faq/index.jsx
+++ src/scenes/faq/index.jsx
@@ -1,18 +1,79 @@
-import { Box, useTheme, Typography } from "mui/material/Accordion";
+import { Box, useTheme } from "@mui/material";
 import Header from "../../components/Header";
-import { Accordion } from "@mui/material/Accordion";
+import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Typography from "@mui/material/Typography";
 import { tokens } from "../../theme";
 
 const FAQ = () => {
-  const theme = useTheme;
+  const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
   return (
     <Box m='20px'>
       <Header title='FAQ' subtitle='Frequently Asked Questions Page' />
+
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant='h5'>
+            An important question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione,
+            totam sapiente! Minus, amet accusantium at inventore cumque commodi
+            vitae maxime!
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant='h5'>
+            Also an important question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione,
+            totam sapiente! Minus, amet accusantium at inventore cumque commodi
+            vitae maxime!
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant='h5'>
+            Another important question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione,
+            totam sapiente! Minus, amet accusantium at inventore cumque commodi
+            vitae maxime!
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant='h5'>
+            Final important question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione,
+            totam sapiente! Minus, amet accusantium at inventore cumque commodi
+            vitae maxime!
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
     </Box>
   );
 };
