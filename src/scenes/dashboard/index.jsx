e2564ea4b87655be57a7c201da5015a8369a37f3
krzysztof baranski <krisbaranski@gmail.com>
2023-04-14 15:44:27 +0200

    refactor: add/create row 3 with charts with Campaign, Sales, Geography Chart

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -221,7 +221,67 @@ const Dashboard = () => {
         </Box>
 
         {/* ROW 3 */}
-        <Box></Box>
+        {/* CAMPAIGN */}
+        <Box
+          gridColumn='span 4'
+          gridRow='span 2'
+          backgroundColor={colors.primary[400]}
+          p='30px'
+        >
+          <Typography variant='h5' fontWeight='600'>
+            Campaign
+          </Typography>
+          <Box
+            display='flex'
+            flexDirection='column'
+            alignItems='center'
+            mt='15px'
+          >
+            <ProgressCircle size='125' />
+            <Typography
+              variant='h5'
+              color={colors.greenAccent[500]}
+              sx={{ mt: "15px" }}
+            >
+              $48,352 revenue generated
+            </Typography>
+            <Typography>Includes extra misc expenditures and costs</Typography>
+          </Box>
+        </Box>
+
+        {/* Sales Quantity */}
+        <Box
+          gridColumn='span 4'
+          gridRow='span 2'
+          backgroundColor={colors.primary[400]}
+        >
+          <Typography
+            variant='h5'
+            fontWeight='600'
+            sx={{ p: "30px 30px 0 30px" }}
+          >
+            Sales Quantity
+          </Typography>
+          <Box height='250px' mt='-20px'>
+            <BarChart isDashboard={true} />
+          </Box>
+        </Box>
+
+        {/*  GEOGRAPHY*/}
+        <Box
+          gridColumn='span 4'
+          gridRow='span 2'
+          backgroundColor={colors.primary[400]}
+          p='30px'
+        >
+          <Typography variant='h5' fontWeight='600' sx={{ mb: "15px" }}>
+            Geography Based Traffic
+          </Typography>
+
+          <Box height='200px'>
+            <GeographyChart isDashboard={true} />
+          </Box>
+        </Box>
       </Box>
     </Box>
   );
