7834f5e7e96bbe67bc1663811cdf692bab780041
krzysztof baranski <krisbaranski@gmail.com>
2023-04-20 18:09:01 +0200

    refactor: import img from assets to profile

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -16,6 +16,7 @@ import PieChartOutlinedIcon from "@mui/icons-material/PieChartOutlined";
 import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
+import User from "../../assets/user.png";
 
 const Item = ({ title, to, icon, selected, setSelected }) => {
   const theme = useTheme();
@@ -95,7 +96,7 @@ const Sidebar = () => {
                   alt='profile-user'
                   width='100px'
                   height='100px'
-                  src={`../../assets/user.png`}
+                  src={User}
                   style={{ cursor: "pointer", borderRadius: "50%" }}
                 />
               </Box>
